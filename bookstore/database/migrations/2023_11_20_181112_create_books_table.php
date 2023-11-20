479124b08be40e63296c7c2bebd5e8edd3d94307
Holylogus <hamar.patrik94@gmail.com>
2023-11-20 19:20:07 +0100

    book_model és book_migration kitöltése

---
+++ bookstore/database/migrations/2023_11_20_181112_create_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id('book_id');
+            $table->string('cim');
+            $table->date('megjeneles_datuma');
+            $table->string('mufaj');
+            $table->string('iroja');
+            $table->integer('ar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
